3c1b11fd58b83529ee116e7d1505370823856e3f
dhedal <hedgardavid@yahoo.fr>
2024-04-11 19:46:11 +0200

    déplacement des pages signin et signup dans le dossier auth, ajout de la account et sa configuration de route de navigation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,9 @@ import Route from "./route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", ""),
     new Route("/galerie", "Galerie", "/pages/Galerie.html", ""),
-    new Route("/signin", "Connexion", "/pages/signin.html", ""),
-    new Route("/signup", "Inscription", "/pages/signup.html", ""),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ""),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ""),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html", ""),
 ];
 
 // Le titre s'affiche comme ceci: Route.titre - websitename
