9a817485210e7dfa8290067283616eb0654fb569
dhedal <hedgardavid@yahoo.fr>
2024-04-16 11:47:35 +0200

    gestion des accès au pages en fonction du role de l'user

--- Router/route.js
+++ Router/route.js
@@ -1,8 +1,9 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJs = ""){
+    constructor(url, title, pathHtml, autorize, pathJs = ""){
         this.url = url;
         this.title = title;
         this.pathHtml = pathHtml;
         this.pathJs = pathJs;
+        this.authorize = autorize;
     }
 }
