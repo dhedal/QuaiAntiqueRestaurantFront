d573855560d560200a404050bfc65e3d04e71b83
dhedal <hedgardavid@yahoo.fr>
2024-04-16 07:07:39 +0200

    validation du champs mot de passe et de sa confirmation, et message feedback valid ou invalid pour chaque champs du formulaire d'inscription

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -2,7 +2,7 @@ const nomInput = document.getElementById("nomInput");
 const prenomInput = document.getElementById("prenomInput");
 const emailInput = document.getElementById("emailInput");
 const passwordInput = document.getElementById("passwordInput");
-const validatePasswordInput = document.getElementById("validatePasswordInput"); 
+const confirmationPasswordInput = document.getElementById("confirmationPasswordInput"); 
 const btnValidationForm = document.getElementById("btn-validation-inscription");
 btnValidationForm.disabled = true;
 
@@ -23,10 +23,10 @@ const validateForm = () => {
     const nomValid = validateRequired(nomInput);
     const prenomValid = validateRequired(prenomInput);
     const mailValid = validEmail(emailInput);
-    validateRequired(passwordInput);
-    validateRequired(validatePasswordInput);
+    const passwordValid = validPassword(passwordInput);
+    const confirmatinoPasswordValid = validConfirmationPasswordInput(passwordInput, confirmationPasswordInput);
     
-    if(nomValid && prenomValid && mailValid) btnValidationForm.disabled = false;
+    if(nomValid && prenomValid && mailValid && passwordValid && confirmatinoPasswordValid) btnValidationForm.disabled = false;
     else btnValidationForm.disabled = true;
 };
 
@@ -45,10 +45,38 @@ const validEmail = ( input)=> {
     }
 };
 
+const validConfirmationPasswordInput = (inputPassword, inputConfirmationPassword) => {
+    if(inputPassword.value == inputConfirmationPassword.value){
+        inputConfirmationPassword.classList.add("is-valid");
+        inputConfirmationPassword.classList.remove("is-invalid");
+        return true;
+    }
+    else {
+        inputConfirmationPassword.classList.add("is-invalid");
+        inputConfirmationPassword.classList.remove("is-valid");
+        return false;
+    }
+};
+
+const validPassword = (input) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W])[A-Za-z\d\W]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)) {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
+    }
+};
+
 nomInput.addEventListener("keyup", validateForm);
 prenomInput.addEventListener("keyup", validateForm);
 emailInput.addEventListener("keyup", validateForm);
 passwordInput.addEventListener("keyup", validateForm);
-validatePasswordInput.addEventListener("keyup", validateForm);
+confirmationPasswordInput.addEventListener("keyup", validateForm);
 
 
