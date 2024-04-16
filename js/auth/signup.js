764402c7ad0c932f5fdd4950b7a1175d1897e799
dhedal <hedgardavid@yahoo.fr>
2024-04-16 06:25:27 +0200

    mis en place de la validation du champs email dans la page signup

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -3,24 +3,46 @@ const prenomInput = document.getElementById("prenomInput");
 const emailInput = document.getElementById("emailInput");
 const passwordInput = document.getElementById("passwordInput");
 const validatePasswordInput = document.getElementById("validatePasswordInput"); 
+const btnValidationForm = document.getElementById("btn-validation-inscription");
+btnValidationForm.disabled = true;
 
 const validateRequired = (input) => {
     if(input.value != "") {
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
+        return true;
     }
     else {
         input.classList.add("is-invalid");
         input.classList.remove("is-valid");
+        return false;
     }
 };
 
 const validateForm = () => {
-    validateRequired(nomInput);
-    validateRequired(prenomInput);
-    validateRequired(emailInput);
+    const nomValid = validateRequired(nomInput);
+    const prenomValid = validateRequired(prenomInput);
+    const mailValid = validEmail(emailInput);
     validateRequired(passwordInput);
     validateRequired(validatePasswordInput);
+    
+    if(nomValid && prenomValid && mailValid) btnValidationForm.disabled = false;
+    else btnValidationForm.disabled = true;
+};
+
+const validEmail = ( input)=> {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if(mailUser.match(emailRegex)) {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
+    }
 };
 
 nomInput.addEventListener("keyup", validateForm);
